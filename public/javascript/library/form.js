43f82e0b05efb6fef0e66674479615b5f48f4098
yumezz <s104213052@mail1.ncnu.edu.tw>
2017-12-30 11:59:42 +0800

    adjustment library

--- public/javascript/library/form.js
+++ public/javascript/library/form.js
@@ -10,11 +10,11 @@ $( "#library-frog" ).click(function(e) {
         jQuery.each(result.LibraryDatas, function(i, val) {
             var searchResultHTML = "<div>";
             searchResultHTML += "<table id='" + val.ID +  "' border='1'><tbody>";
-            searchResultHTML += "<tr><td>科目</td><td class='dataFamily'>" + val.Family + "</td></tr>";
-            searchResultHTML += "<tr><td>種類</td><td class='dataOrganismName'>"+ val.OrganismName +"</td></tr>";
-            searchResultHTML += "<tr><td>性狀</td><td class='dataStatus'>" + val.Status + "</td></tr>";
-            searchResultHTML += "<tr><td>棲地</td><td class='dataHabitat'>" + val.Habitat + "</td></tr>";
-            searchResultHTML += "<tr><td>操作</td><td><button class='deleteDataButton' value='" + val.ID + "'>刪除</button><button class='modifyDataButton' value='" + val.ID + "'>編輯</button><button class='getRecordsByOrganismName' value='" + val.OrganismName + "'>相關記錄</button></td></tr>";
+            searchResultHTML += "<tr><td class='title'>科目</td><td class='dataFamily'>" + val.Family + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>種類</td><td class='dataOrganismName'>"+ val.OrganismName +"</td></tr>";
+            searchResultHTML += "<tr><td class='title'>性狀</td><td class='dataStatus'>" + val.Status + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>棲地</td><td class='dataHabitat'>" + val.Habitat + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>操作</td><td><button class='deleteDataButton' value='" + val.ID + "'>刪除</button><button class='modifyDataButton' value='" + val.ID + "'>編輯</button><button class='getRecordsByOrganismName' value='" + val.OrganismName + "'>相關記錄</button></td></tr>";
             searchResultHTML += "</tbody></table>";
             searchResultHTML += "</div></br>";
             searchResultHTML += '';
@@ -35,11 +35,11 @@ $( "#library-lepidoptera" ).click(function(e) {
         jQuery.each(result.LibraryDatas, function(i, val) {
             var searchResultHTML = "<div>";
             searchResultHTML += "<table id='" + val.ID +  "' border='1'><tbody>";
-            searchResultHTML += "<tr><td>科目</td><td class='dataFamily'>" + val.Family + "</td></tr>";
-            searchResultHTML += "<tr><td>種類</td><td class='dataOrganismName'>"+ val.OrganismName +"</td></tr>";
-            searchResultHTML += "<tr><td>性狀</td><td class='dataStatus'>" + val.Status + "</td></tr>";
-            searchResultHTML += "<tr><td>棲地</td><td class='dataHabitat'>" + val.Habitat + "</td></tr>";
-            searchResultHTML += "<tr><td>操作</td><td><button class='deleteDataButton' value='" + val.ID + "'>刪除</button><button class='modifyDataButton' value='" + val.ID + "'>編輯</button><button class='getRecordsByOrganismName' value='" + val.OrganismName + "'>相關記錄</button></td></tr>";
+            searchResultHTML += "<tr><td class='title'>科目</td><td class='dataFamily'>" + val.Family + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>種類</td><td class='dataOrganismName'>"+ val.OrganismName +"</td></tr>";
+            searchResultHTML += "<tr><td class='title'>性狀</td><td class='dataStatus'>" + val.Status + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>棲地</td><td class='dataHabitat'>" + val.Habitat + "</td></tr>";
+            searchResultHTML += "<tr><td class='title'>操作</td><td><button class='deleteDataButton' value='" + val.ID + "'>刪除</button><button class='modifyDataButton' value='" + val.ID + "'>編輯</button><button class='getRecordsByOrganismName' value='" + val.OrganismName + "'>相關記錄</button></td></tr>";
             searchResultHTML += "</tbody></table>";
             searchResultHTML += "</div></br>";
             searchResultHTML += '';
