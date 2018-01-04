23d79dd6a6c3fbe63bd96ca76f8b74affbc5eb63
blacksheep <shiptina@gmail.com>
2018-01-05 01:07:55 +0800

    相簿

--- public/javascript/gallery/form.js
+++ public/javascript/gallery/form.js
@@ -1,3 +1,95 @@
+$( "#records-frog" ).click(function(e) {
+    $('#records-data').empty();
+    $.ajax({
+        type: "GET",
+        url: "/gallery/data",
+        data: { searchtype: "tag", keyword:"frog" }
+    }).done(function( searchResult ) {
+        var result = $.parseJSON(searchResult);
+        console.log(result);
+        //console.log(result);
+//var index;
+//        var photoSrc;
+        var key = [];
+        var photos=[];
+        
+        jQuery.each(result.GroupByTag, function(name) {
+            key.push(name);
+        });
+        //console.log(key);
+        var imgDivHTML = "";
+        
+        //var image = Object.keys(result.GroupByTag);
+        //console.log(result.GroupByTag);
+        //console.log(key);
+        for(var i =0; i<key.length; i++){
+            console.log(result.GroupByTag[key[i]][0]);
+            photos.push(result.GroupByTag[key[i]][0]);
+        }
+        //console.log(photos);
+        
+        for(var i =0; i<photos.length; i++){
+            imgDivHTML += "<button class='showAlbum' value='" + key[i] + "'><img  width='200' height='200' src='/storage/photo/" + photos[i]+"' style='padding:10px;''>"+key[i]+"</button>";
+        }
+//       for(var i=0;i<key.length;i++){
+//           console.log(i);
+//            jQuery.each(result.GroupByTag[key[i]], function(index, photoSrc){
+//                photos.push(photoSrc);
+//                //console.log(photos[0]);
+//              });
+//                imgDivHTML += "<botton value='" + key[i] + "'><img class='showAlbum' width='200' height='200' src='/storage/photo/" + photos[i]+"' style='padding:10px;''>"+key[i]+"</botton>";    
+//            
+//             
+//       }
+        $('#records-data').prepend(imgDivHTML);
+        $(".showAlbum").click(function(e) {
+            $('#album-ul').empty();
+            var HTML ="";
+            var number=$(this).val();
+            var image=[];
+            image.push(result.GroupByTag[number]);
+                console.log(image);        
+             for(var i =0; i<image[0].length; i++){
+             HTML += "<li><img width='400' height='400' src='/storage/photo/" + image[0][i]+"'></li>";
+             
+             } 
+            console.log(HTML);
+             $('#album-ul').prepend(HTML);    
+            $('.album-ul').css('display', 'flex');
+            $('#album').show().css('display', 'flex');
+            $(".single-album-div ul li").first().show();            
+            });  
+        
+    });
+});
+$(".next").click(function(){
+    if ($(".album-ul li:visible").next().length != 0)
+        $(".album-ul li:visible").next().show().prev().hide();
+    else {
+        $(".album-ul li:visible").hide();
+        $(".album-ul li:first").show();
+        return false;
+    }
+});
+
+$(".prev").click(function(){
+    if ($(".album-ul li:visible").prev().length != 0)
+        $(".album-ul li:visible").prev().show().next().hide();
+    else {
+        $(".album-ul li:visible").hide();
+        $(".album-ul li:last").show();
+        return false;
+    }
+});
+$( "#close-album-div-button" ).click(function(e) {
+      $( "#album" ).hide();
+  });
+
+
+
+
+
+
 /*
 $( "#records-frog" ).click(function(e) {
     $.ajax({
@@ -72,18 +164,20 @@ $( "#close-album-div-button" ).click(function(e) {
 
 //下面這段先留著 別刪！
 
-$( "#records-frog" ).click(function(e) {
+/*$( "#records-frog" ).click(function(e) {
     $.ajax({
         type: "GET",
         url: "/gallery/data",
         data: { searchtype: "tag", keyword:"frog" }
     }).done(function( searchResult ) {
         var result = $.parseJSON(searchResult);
-        console.log(result)
+        console.log(result);
         var HTML = "<div>";
         HTML += "<table border='1'><tbody>";
+        
         jQuery.each(result.GroupByTag, function(name) {
             HTML += "<tr><td class='dataOrganismName'>" + "<button class='showAlbum' value='" + name + "' >" + name + "</button></td></tr>";
+            
         });
         HTML += "</tbody></table>";
         HTML += "</div></br>";
@@ -148,4 +242,4 @@ $(".prev").click(function(){
         $(".album-ul li:last").show();
         return false;
     }
-});
+});*/
